feea8e22452af973920a21a97307d523d3de5156
Dillon Henn <dillon.henn.f2ks@statefarm.com>
2020-06-05 13:22:24 -0700

    Update inputs.tf

--- inputs.tf
+++ inputs.tf
@@ -1,6 +1,5 @@
 variable "upload_artifacts" {
   type        = string
-  default     = "./build" # sample ReactJS build folder
   description = "Artifact/Directory/File path to be uploaded relative to the Terraform directory (i.e ./build, ./dist, index.html, etc...)"
 }
 
