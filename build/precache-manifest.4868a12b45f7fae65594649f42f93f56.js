292263fabe9d9f2419e788c1529100e3be68763c
Dillon Henn <dh329@nau.edu>
2020-06-05 12:31:58 -0700

    sample build folder

---
+++ build/precache-manifest.4868a12b45f7fae65594649f42f93f56.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f1e955f3d77aea5e13d083b87bcc90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "107fd56411a2692bc0a8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "107fd56411a2692bc0a8",
+    "url": "/static/js/main.5f246e29.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
